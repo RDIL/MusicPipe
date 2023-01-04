09e065090258251e9ff21d770c64b2f64ff3f6bb
Reece Dunham <me@rdil.rocks>
2023-01-03 20:55:13 -0500

    Remove pnp loose mode fallbacks

--- src/basicApiHandler.ts
+++ src/basicApiHandler.ts
@@ -3,9 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next"
 export class BasicApiHandler<ApiType> {
     dispatch(req: NextApiRequest, res: NextApiResponse<ApiType | string>) {
         switch (req.method) {
-            case "GET":
-                this.get(req, res)
-                break
             case "POST":
                 this.post(req, res)
                 break
@@ -21,31 +18,6 @@ export class BasicApiHandler<ApiType> {
                 )
                 return
         }
-    }
-
-    async get(req: NextApiRequest, res: NextApiResponse<ApiType | string>) {
-        res.status(500).send("Not implemented")
-
-        // const id = req.query.id as string
-        //
-        // const entity: BaseType | null = await this.dataSource.manager.findOne(
-        //     this.entity,
-        //     {
-        //         where: {
-        //             // @ts-expect-error Whatever
-        //             id,
-        //         },
-        //         // @ts-expect-error Whatever
-        //         select: this.keys,
-        //     }
-        // )
-        //
-        // if (entity) {
-        //     res.status(200).json(entity.toJSON())
-        //     return
-        // }
-        //
-        // res.status(404).send(`No ${this.entity.name} found with id: ${id}`)
     }
 
     async post(req: NextApiRequest, res: NextApiResponse<ApiType | string>) {
